61c6a87a2784fd3afb420e462d43e1f08444d456
sajad mohammadi <daram3118@gmail.com>
2022-11-21 20:08:33 +0330

    first init

---
+++ app/Enums/SMSTemplate.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Enums;
+
+
+abstract class SMSTemplate
+{
+    const BANK_ACCOUNT = 'mehrino-bankaccount';
+    const PAYMENT = 'mehrino-payment';
+    const DONATE = 'mehrino-donate';
+    const TICKET = 'mehrino-ticket';
+    const VERIFY = 'mehrino-verify';
+    const WITHDRAW = 'mehrino-withdraw';
+    const MILAD = 'milad';
+}
